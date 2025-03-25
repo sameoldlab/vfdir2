95e8163da62d8bffa623c3f872a42339d6d23fd3
ibro <sameoldlab@proton.me>
2025-03-25 21:22:20 +0100

    remove local cert setup
    no longer needed outside browser

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { readFileSync } from 'fs'
-import { loadEnv } from 'vite'
 
 const target = 'es2022'
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ }) => ({
 	server: {
 		port: 5461,
-		https: mode === 'development' ? {
-			key: readFileSync(`${__dirname}/certs/key.pem`),
-			cert: readFileSync(`${__dirname}/certs/cert.pem`)
-		} : null,
-		proxy: {}
 	},
 	esbuild: { target },
 	optimizeDeps: {
