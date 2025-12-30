deca15084fdeb812420672dd960ba4354e3fc7b9
ibro <sameoldlab@proton.me>
2025-12-30 11:12:54 +0100

    fix: env and globals

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -2,12 +2,21 @@
 
 /// <reference types="@sveltejs/kit" />
 
+import type { Did } from "@atcute/lexicons"
+import type { OAuthSession } from "@atcute/oauth-node-client"
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
+declare global {
+	namespace App {
+		interface Locals {
+			user?: OAuthSession[]
+			did?: Did
+		}
+		// interface Platform {}
+		// interface Session {}
+		// interface Stuff {}
+	}
 }
+
