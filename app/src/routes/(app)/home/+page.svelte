60fef4d2cbc3e635b6f932ad2c4a3af621fce083
ibro <sameoldlab@proton.me>
2025-12-30 01:15:05 +0100

    move app into separate folder

--- app/src/routes/(app)/home/+page.svelte
+++ app/src/routes/(app)/home/+page.svelte
@@ -3,6 +3,7 @@
 <script lang="ts">
 	import View from '$lib/components/view.svelte'
 	import { entries } from '$lib/data/maps.svelte'
+	import { subscribeMrks } from '$lib/services/atproto/client'
 	import type { Snapshot } from '@sveltejs/kit'
 
 	const scroll = (init = 0) => {
@@ -27,8 +28,21 @@
 	}
 	let dir = $state('/home/ibro/Documents/ref/oooooooooooooooohh')
 	let files: { name: string; path: string }[] = []
+	// let subs = $state([])
 </script>
 
 <input type="text" bind:value={dir} />
 <View data={files} {y} />
+<button
+	onclick={async () => {
+		const subs = subscribeMrks(['did:plc:ukgwapa3bceculh4cobcopg3'])
+		for await (const ev of subs) {
+			console.log(ev)
 		}
+	}}>Connect Stream</button
+>
+<!-- Firefox can’t establish a connection to the server at wss://jetstream2.us-east.bsk.network/subscribe?requireHello=true&cursor=1766851302286000. -->
+
+<!-- {#each subs as sub} -->
+
+<!-- {/each} -->
---
+++ app/src/routes/(app)/testing/types/+page.svelte
@@ -0,0 +1,44 @@
+<!-- SPDX-License-Identifier: MPL-2.0 -->
+
+<script lang="ts">
+	import GridView from '$lib/components/views/GridView.svelte'
+	import { pool } from '$lib/database/connectionPool.svelte'
+
+	const types = pool.query(
+		`select * from blocks b where b.id in (
+			select min(id) from blocks
+			group by type
+			);`,
+		undefined
+	)
+	let selected = $state({})
+	const select = (e) => {
+		selected = e
+	}
+</script>
+
+<!-- prettier-ignore -->
+<div class="item">
+	{#each types.data as { ...item }}
+		| <button onclick={() => select(item)}>{item.type}</button> &nbsp;
+	{/each}|
+<pre><code>{#each Object.entries(selected) as [k, v]}{#if v}{k}: {v}<br/>{/if}{/each}</code></pre>
+</div>
+
+<GridView {...types.data} />
+
+<style>
+	button {
+		background: none;
+		padding: 0;
+	}
+	pre {
+		height: 12rem;
+		overflow: auto;
+		margin: 0.25rem;
+	}
+	code {
+		text-wrap: wrap;
+		line-break: strict;
+	}
+</style>
