0df2dcf7bb7059e8c522fbcdca8b17bb940d6cf9
ibro <sameoldlab@proton.me>
2025-12-31 22:32:54 +0100

    change adapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -15,9 +15,7 @@ export default {
   },
 
   kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    }),
+    adapter: adapter(),
     experimental: {
       remoteFunctions: true
     }
