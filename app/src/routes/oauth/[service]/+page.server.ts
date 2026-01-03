e24f078d0f4021cad981dae31e50e46f3aa37d3a
ibro <sameoldlab@proton.me>
2026-01-03 20:05:16 +0100

    merge oath2 service endpoints

--- app/src/routes/oauth/[service]/+page.server.ts
+++ app/src/routes/oauth/[service]/+page.server.ts
@@ -3,13 +3,7 @@ import { getOauth2Url, startOAuthFlow } from '$lib/server/auth/manager';
 import type { Actions } from './$types';
 
 export const actions = {
-  default: async ({ locals }) => {
-    const ctx = locals.ctx;
-    console.log({ locals })
-
-    const authUrl = getOauth2Url(ctx, 'arena');
-    console.log({ authUrl })
-
-    throw redirect(302, authUrl);
+  default: async ({ locals, params }) => {
+    throw redirect(302, getOauth2Url(locals.ctx, params.service));
   }
 } satisfies Actions;
--- app/src/routes/oauth/[service]/callback/+page.server.ts
+++ app/src/routes/oauth/[service]/callback/+page.server.ts
@@ -2,9 +2,8 @@ import { redirect } from '@sveltejs/kit';
 import { handleOAuth2Callback } from '$lib/server/auth/manager';
 import type { PageServerLoad } from '../$types';
 
-export const load: PageServerLoad = async ({ url, locals }) => {
-	console.log(locals)
-	await handleOAuth2Callback(locals.ctx, 'arena', locals.deviceUid, url.searchParams)
+export const load: PageServerLoad = async ({ url, locals, params }) => {
+	await handleOAuth2Callback(locals.ctx, params.service, locals.deviceUid, url.searchParams)
 
 	redirect(302, '/');
 }
