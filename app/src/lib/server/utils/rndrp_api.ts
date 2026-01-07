beb2dc415672fa56900bed76131cf1892eef6546
ibro <sameoldlab@proton.me>
2026-01-07 13:44:28 +0100

    fix: define options before setting propeerties

--- app/src/lib/server/utils/rndrp_api.ts
+++ app/src/lib/server/utils/rndrp_api.ts
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MPL-2.0
 
 export const query = async <T>(path: string, options?: RequestInit, auth_token?: string) => {
-	if (auth_token) options.headers['Authorization'] = 'Bearer ' + auth_token
+	let headers: HeadersInit = {}
+	if (auth_token) headers['Authorization'] = 'Bearer ' + auth_token
 
 	let resp = await fetch(`https://api.raindrop.io/rest/v1${path}`, {
 		...options,
 		headers: {
 			...options.headers,
+			...headers,
 			'Content-Type': 'application/json',
 		},
 	})
