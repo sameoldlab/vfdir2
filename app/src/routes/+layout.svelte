4fe1e57487f8cfefda96d0476dac3f7a4a06c297
ibro <sameoldlab@proton.me>
2026-01-17 18:44:43 +0100

    initialize data store

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,13 +2,32 @@
 
 <script lang="ts">
 	import { env } from '$env/dynamic/public'
+	import { onMount } from 'svelte'
 	import '../app.css'
 	import { setupConvex } from 'convex-svelte'
+	import { pool } from '$lib/database/connectionPool.svelte'
+	import { initStore } from '$lib/database/createTables'
+	import { watchEvents, bootstrap } from '$lib/database/watchEvents'
 	let { children } = $props()
 
-	setupConvex(env.PUBLIC_CONVEX_URL)
+	// setupConvex(env.PUBLIC_CONVEX_URL)
+	let ready = $state(false)
+	onMount(() => {
+		console.debug('bootstrapping...')
+		pool.exec(async (tx) => {
+			await initStore(tx)
+			// const { watchEvents, bootstrap } =
+			// 	await import('$lib/database/watchEvents')
+			watchEvents()
+			await bootstrap(tx)
+			ready = true
+		})
+	})
 </script>
-{@render children()}
+{ready}
+{#if ready}
+	{@render children()}
+{/if}
 
 <style>
 	:global(div#contents) {
