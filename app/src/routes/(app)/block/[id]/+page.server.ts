0500df8e30f26c364cd7105be0871bebbda93b90
ibro <sameoldlab@proton.me>
2026-01-18 22:35:34 +0100

    request individual blocks

---
+++ app/src/routes/(app)/block/[id]/+page.server.ts
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MPL-2.0
+import { arenaClient } from "$lib/services/arena/client"
+import type { PageServerLoad } from "./$types"
+
+export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+  if (!!Number(params.id)) {
+    // locals.
+    const { error, data: block } = await arenaClient.GET('/v3/blocks/{id}', {
+      params: {
+        path: { id: Number(params.id) },
+
+      },
+      fetch,
+    })
+
+    return { block, error }
+  }
+}
--- app/src/routes/(app)/block/[id]/+page.svelte
+++ app/src/routes/(app)/block/[id]/+page.svelte
@@ -4,16 +4,49 @@
 	import { page } from '$app/state'
 	import BlockDetails from '$lib/components/BlockDetails.svelte'
 	import { entries } from '$lib/data/maps.svelte'
-	const block = $derived(entries.get(page.params.id))
+	import { Block } from '$lib/data/block.svelte.js'
+	import { untrack } from 'svelte'
+	const { data } = $props()
+
+	const block = $derived(entries.get(page.params.id!))
+	console.log({ data, block})
+	$effect(() => {
+		if (!data.block) return
+		untrack(() => {
+			const b = new Block(Block.fromArena(data.block))
+			console.log({b: entries.get(page.params.id!)})
+		})
+	})
 </script>
 
 <div>
-	<BlockDetails {block} />
+	{#if block}
+		<BlockDetails {block} />
+	{:else if data.block}
+		caching block...
+	{:else}
+		<div class="error">
+			<h1>Error</h1>
+			{#if data.error}
+				{#if 'code' in data}
+					{#if data.code === 404}
+						The block {page.params.id} does not exist in are.na <br />
+					{:else if data.code === 401 || data.code === 403}
+						<p>You do not have permission to access this block.</p>
+						<a class="link" href="/accounts">Connect your are.na account</a>
+					{/if}
+				{/if}
+			{:else}
+				<p>No entry found for id: <code>{page.params.id}</code></p>
+				<p>Attempting fetch from are.na</p>
+			{/if}
+		</div>
+	{/if}
 </div>
 
 <style>
 	div {
-		overflow-y: scroll;
+		overflow-y: auto;
 		height: 100%;
 	}
 </style>
