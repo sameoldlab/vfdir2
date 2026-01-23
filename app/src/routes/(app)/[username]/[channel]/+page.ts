e5315bcc20e2a4439d420db338472613971d78c5
ibro <sameoldlab@proton.me>
2026-01-23 23:08:13 +0100

    remove v2 fn calls

--- app/src/routes/(app)/[username]/[channel]/+page.ts
+++ app/src/routes/(app)/[username]/[channel]/+page.ts
@@ -1,13 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
-import { browser } from "$app/environment"
 import type { PageLoad } from "./$types"
 
 export const load: PageLoad = ({ params }) => {
-  if (!browser) return {}
-  import("$lib/services/arena/arenav2").then(({ getBlocks }) => {
-    console.log('echo', params)
-    getBlocks(params.channel)
-  })
   return {}
 }
