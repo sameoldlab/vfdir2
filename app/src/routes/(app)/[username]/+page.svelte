d8202a211a39b86a67f16df041cfd5affd4547a3
ibro <sameoldlab@proton.me>
2026-01-23 23:17:37 +0100

    tmp: convert pull from remote function

--- app/src/routes/(app)/[username]/+page.svelte
+++ app/src/routes/(app)/[username]/+page.svelte
@@ -3,17 +3,37 @@
 <script lang="ts">
 	import { page } from '$app/state'
 	import View from '$lib/components/view.svelte'
-	import { api } from '$lib/convex/_generated/api'
-	// import { users } from '$lib/data/maps.svelte'
-	import { spiderUser } from '$lib/queries/listRecords.remote'
-	import { isActorIdentifier, isDid } from '@atcute/lexicons/syntax'
+	import { Block } from '$lib/data/block.svelte.js'
+	import { Channel } from '$lib/data/channel.svelte.js'
+	import { users } from '$lib/data/maps.svelte'
+	import { pullCosmik } from '$lib/services/atpro/pullCosmik'
 	import type { Snapshot } from '@sveltejs/kit'
-	import { useQuery } from 'convex-svelte'
-	import { ConvexError } from 'convex/values'
 	import { untrack } from 'svelte'
 
 	const { data } = $props()
-	const username = $derived(data.service === 'atproto' ? data.actor.did : page.params.username)
+	$inspect(data)
+	const username = $derived(
+		data.service === 'atproto' ? data.actor.did : page.params.username!
+	)
+
+	$effect(() => {
+		if (!data.contents) return
+		data.contents.data
+		untrack(() => {
+			data.contents.data.forEach((e) => {
+				if (e.base_type === 'Block') {
+					new Block(Block.fromArena(e))
+					return
+				}
+				if (e.base_type === 'Channel') {
+					new Channel(Channel.fromArena(e))
+				}
+			})
+		})
+	})
+
+	const user = $derived(users.get(username))
+	$inspect(user)
 
 	// const user = $derived(users.get(page.params.username))
 	// const data = $derived(user?.entries)
@@ -37,18 +57,11 @@
 			y.val = value
 		}
 	}
-	const cvx_entries = $derived(useQuery(api.data.get_user_entries, {
-		/* pagination: {
-			numItems: 0,
-			cursor: null
-		}, */
-		userId: username ?? ''
-	}))
 
 	$effect(() => {
 		if (data.service === 'atproto')
 			untrack(() => {
-				spiderUser({ did: data.actor.did, pds: data.actor.pds })
+				pullCosmik(data.actor)
 			})
 	})
 </script>
@@ -56,24 +69,21 @@
 <svelte:head>
 	<title>{data.actor?.handle ?? username} | vfdir</title>
 </svelte:head>
-{#if cvx_entries.isLoading}
-	loading...
-{:else if cvx_entries.error}
+
+{#if !user}
 	<div class="error">
-		{#if (cvx_entries.error as ConvexError<{ message: string; code: number }>).data.message === 'user not found'}
-			Could not find user: <code>{username}</code>.<br />
-			{#if data.service === 'atproto'}
-				Are you sure this did is correct? <a
-					href="https://pdsls.dev/at://{username}#collections:network.cosmik"
-					>Check on pdsls.dev</a
-				>
-			{:else}
-				If this is an are.na user, try searching one of their channels instead.
-			{/if}
+		User: {username} not found. Try searching one of their channels instead.
+		{#if data.service === 'atproto'}
+			Are you sure this did is correct? <a
+				href="https://pdsls.dev/at://{username}#collections:network.cosmik"
+				>Check on pdsls.dev</a
+			>
 		{:else}
-			{cvx_entries.error}
+			If this is an are.na user, try searching one of their channels instead.
 		{/if}
 	</div>
-{:else if cvx_entries.data}
-	<View data={cvx_entries.data} {y} />
+{:else if user.entries?.length === 0}
+	empty
+{:else}
+	<View data={user.entries} {y} />
 {/if}
