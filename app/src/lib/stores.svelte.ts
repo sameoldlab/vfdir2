c71b3766c609c9e36e900566929503db3501ebf0
ibro <sameoldlab@proton.me>
2026-01-29 10:27:53 +0100

    create route ctx

--- app/src/lib/stores.svelte.ts
+++ app/src/lib/stores.svelte.ts
@@ -2,9 +2,22 @@
 
 import { createContext } from "svelte";
 import type { NavigationTarget } from "@sveltejs/kit";
+import type { ArenaUser } from "./services/arena/types";
+import type { ResolvedActor } from "@atcute/identity-resolver";
 
 export const VIEWS = ['block', 'miller', 'table', 'canvas'] as const
 export type VIEWS = typeof VIEWS[number]
 
 type TreeNode = NavigationTarget
 export const [getTree, setTree] = createContext<TreeNode[]>()
+
+export type RouteCtx = {
+  user: ArenaUser,
+  service: 'arena',
+} | {
+  user: ResolvedActor,
+  service: 'atproto',
+} | {
+  service: 'raindrop',
+}
+export const [getRouteCtx, setRouteCtx] = createContext<RouteCtx>()
