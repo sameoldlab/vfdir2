d79f328472811a4692d1a7487f8d1ca078dcb88a
ibro <sameoldlab@proton.me>
2026-02-05 21:25:37 +0100

    add auth token

--- app/src/routes/(app)/block/[id]/+page.server.ts
+++ app/src/routes/(app)/block/[id]/+page.server.ts
@@ -2,14 +2,23 @@
 import { arenaClient } from "$lib/services/arena/client"
 import { error } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
+import { env } from "$env/dynamic/private"
 
 export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+
   if (!!Number(params.id)) {
-    // locals.
+    const auth = await locals.ctx.convex.query(locals.ctx.authApi.getServiceConnection, {
+      cvx_secret: env.SERVER_SECRET,
+      sessionKey: locals.sessionKey,
+      service: 'arena'
+    })
+
     const { error, data: block } = await arenaClient.GET('/v3/blocks/{id}', {
       params: {
+        header: {
+          Authorization: 'Bearer ' + auth?.access_key
+        },
         path: { id: Number(params.id) },
-
       },
       fetch,
     })
