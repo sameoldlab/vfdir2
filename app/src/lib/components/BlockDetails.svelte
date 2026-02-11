112b4ee81816ef69b3b9a55e9d2225f52da62d59
ibro <sameoldlab@proton.me>
2026-02-11 06:48:14 +0100

    stop using handleFile as service worker does the same work
    handleFile made more sense when plans for using a local filesystem
    service were still in progress. I no longer believe this is a godd idea
    inside a web browser.

--- app/src/lib/components/BlockDetails.svelte
+++ app/src/lib/components/BlockDetails.svelte
@@ -9,121 +9,132 @@
 
 	let { block: b }: { block: Entry } = $props()
 
-	const content = $derived(b.type === 'text' ? micromark(b.content ?? '') : b.type === 'link' && b.image === '' ? `<p>${b.description}</p>` : null)
+	const content = $derived(
+		b.type === 'text'
+			? micromark(b.content ?? '')
+			: b.type === 'link' && b.image === ''
+				? `<p>${b.description}</p>`
+				: null
+	)
 	const genRoute = (e: Entry) => {
 		if (e.key.includes('/')) return `/${e.key}`
 		return `/${e.author.key}/${e.key}`
 	}
 </script>
 
-<article>
-	<div class="block">
-		{#if b.image}
-			<img
-				use:handleFile={{ src: b.image }}
+<div class="wrapper">
+	<article>
+		<div class="block">
+			{#if b.image}
+				{#key b.image}
+						<!-- use:handleFile={{ src: b.image, pool: getPool() }} -->
+						<!-- data-src={b.image} -->
+					<img
+						src={b.image}
+						in:fade
+						crossorigin="anonymous"
+						alt={b.image}
+					/>
+				{/key}
+			{:else if b.type === 'attachment'}
+					<!-- use:handleFile={{ src: b.attachment! }}
+					data-src={b.attachment} -->
+				<video
+					src={b.attachment}
+					muted
+					autoplay
+					loop
+					controls
+				></video>
+			{:else if b.type === 'channel'}
+				<div class="channel">
+					<p class="wrapper">{b.title}</p>
+					<p class="author">by {b.author?.name}</p>
+				</div>
 				src={b.image}
-				data-src={b.image}
-				in:fade
-				crossorigin="anonymous"
-				alt={b.image}
-			/>
-		{:else if b.type === 'attachment'}
-			<video
-				use:handleFile={{ src: b.attachment! }}
-				data-src={b.attachment}
-				muted
-				autoplay
-				loop
-				controls
-			></video>
-		{:else if b.type === 'channel'}
-			<div class="channel">
-				<p class="title">{b.title}</p>
-				<p class="author">by {b.author?.name}</p>
-			</div>
-		{:else if content}
-			<div class="text"><p>{@html content}</p></div>
-		{/if}
-	</div>
-	<div>
-		<header>
-			<h1>{b.title}</h1>
-			<p class="description text-6">{b.description}</p>
-		</header>
-		<div class="metadata">
-			<div class="data-item">
-				<p>Type</p>
+			{:else if content}
+				<div class="text"><p>{@html content}</p></div>
+			{/if}
+		</div>
+		<aside>
+			<header>
+				<h1>{b.title}</h1>
+				<p class="description text-6">{b.description}</p>
+			</header>
+			<div class="data">
+				<p class="label">Type</p>
 				<p>{b.type}</p>
-			</div>
-			<div class="data-item">
-				<p>Modified</p>
-				<time datetime={new Date(b.updated_at).toLocaleString()}
-					>{naturalDate(b.updated_at)}</time
-				>
-			</div>
-			<div class="data-item">
-				<p>Added</p>
+				<p class="label">By</p>
+				<a href={'/' + b.author.key}> {b.author.name} </a>
+
+				{#if 'source' in b && b.source}
+					<p class="label">Source</p>
+					<a href={b.source}> {b.title} </a>
+				{/if}
+				<p class="label">Added</p>
 				<time datetime={new Date(b.created_at).toLocaleString()}
 					>{naturalDate(b.created_at)}</time
 				>
-			</div>
-			<div class="data-item">
-				<p>By</p>
-				<a href={'/' + b.author.key}> {b.author.name} </a>
-			</div>
-
-			{#if 'source' in b && b.source}
-				<div class="data-item">
-					<p>Source</p>
-					<a href={b.source}> {b.title} </a>
-				</div>
-			{/if}
-			<div class="data-item">
-				<p>Connections</p>
+				<p class="label">Modified</p>
+				<time datetime={new Date(b.updated_at).toLocaleString()}
+					>{naturalDate(b.updated_at)}</time
+				>
+				<p class="label">Connections</p>
 				<div class="connections">
 					{#each b.connections as channel}
-						<a
-							href={`${genRoute(channel)}#${b.key}`}
-							class="connection"
-						>
-							<span>{channel.title} by {channel.author?.name}</span>
-							<p>{channel.length} blocks</p>
+						<a href={`${genRoute(channel)}#${b.key}`} class="connection">
+							<span
+								>{channel.title} [{channel.length}] by {channel.author
+									?.name}</span
+							>
+							<p></p>
 						</a>
 					{/each}
 				</div>
 			</div>
-		</div>
-		<!--<pre>{JSON.stringify(block).replaceAll(/\{/g, '\n    ')}</pre>-->
-	</div>
-</article>
+		</aside>
+	</article>
+</div>
 
 <style>
-	article {
+	div.wrapper {
+		container-name: wrapper;
+		container-type: inline-size;
 		width: 100%;
 		height: 100%;
-		flex-direction: column;
 		overflow-y: auto;
-		container-name: article;
+		padding: 1em 1em 2em;
+	}
+	article {
+		display: flex;
+		width: 100%;
+		flex-direction: column;
 		margin-inline: auto;
-		padding: 1em 1em 4em;
-		& > div {
-			display: flex;
-			gap: 1em;
-			flex-wrap: wrap;
-			width: 100%;
-			justify-content: space-evenly;
-			align-items: center;
+		gap: 1em;
+	}
+	@container wrapper (width > 800px) {
+		article {
+			flex-direction: row;
 		}
 	}
 	div.block {
-		flex-grow: 1;
+		display: flex;
+		gap: 1em;
+		flex-wrap: wrap;
+		overflow-x: auto;
+		justify-content: space-evenly;
+		align-items: center;
 		display: flex;
 		justify-content: center;
 		padding-block: 1rem;
 	}
 	img,
 	video {
-		width: min(100%, 700px);
+		width: 1rem;
+		width: clamp(5rem, 50cqi, 700px);
+	}
+	aside {
+		max-width: min(60ch, 100%);
 	}
 	h1 {
 		font-size: 1rem;
@@ -131,31 +142,28 @@
 		padding-block-end: 0.25em;
 	}
 	header {
-		width: 100%;
+		padding-block: 1rem 2rem;
+	}
+	.data {
+		display: grid;
+		grid-template-columns: min-content 1fr;
+		column-gap: 1rem;
+		background-color: var(--b1);
+		padding: 1rem 0.5rem;
 	}
 	.metadata {
 		margin-inline-start: auto;
 		margin-inline: auto;
 		width: 100%;
 		padding-inline-end: 1rem;
-	}
-	.data-item {
 		display: grid;
-		grid-template-columns: 1fr 2fr;
-		justify-content: space-between;
-		padding-block: 0.25rem;
-		border-block-end: var(--border);
-		&:first-child {
-			border-block-start: var(--border);
-		}
-		p:first-child {
-			color: var(--b6);
-			font-weight: 500;
-			font-size: 0.95rem;
-		}
-		& > * {
-			padding-block: 0.25rem;
-		}
+		gap: 0.25rem;
+	}
+	.label {
+		text-align: end;
+		color: var(--b4);
+		font-weight: 500;
+		font-size: 0.95rem;
 	}
 	.connections {
 		display: grid;
@@ -166,7 +174,5 @@
 		justify-content: space-between;
 		/* padding-block: 0.5rem; */
 		/* border-block: 1px solid var(--line); */
-	}
-	@container article(width > 400px) {
 	}
 </style>
